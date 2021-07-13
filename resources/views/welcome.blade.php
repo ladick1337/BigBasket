d07b37274338d308e04607d3bc2b255766e9e57b
Evidancer <62230462+Evidancer@users.noreply.github.com>
2021-07-13 23:14:24 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,9 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div class="title m-b-md">
+                    framework
+                </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
