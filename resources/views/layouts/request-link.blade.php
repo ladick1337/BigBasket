eeb34fd534c8dd22bbb2990ee0d844e79db1802a
Evidancer <62230462+Evidancer@users.noreply.github.com>
2021-07-14 00:19:24 +0300

    layouts

---
+++ resources/views/layouts/request-link.blade.php
@@ -0,0 +1,15 @@
+<section class="link-form-section">
+    <div class="container">
+        <div class="link-form-section-wrapper">
+            <div class="link-form-tab">
+                <label class="link-form-tab__item link-form-tab__item--active" for="link"> Заказать по ссылке</label>
+                <a class="link-form-tab__item" href="#">Каталог еВау</a>
+                <a class="link-form-tab__item" href="#">Получить адрес в США</a>
+            </div>
+            <form class="link-form" action="#">
+                <input  class="link-form__input" type="text" name="link" id="link" placeholder="Вставьте ссылку, например https://www.amazon.com/dp/B0844JKGSK/">
+                <button class="link-form__btn btn btn--orange" type="submit">Заказать этот товар</button>
+            </form>
+        </div>
+    </div>
+</section>
